f1d7a9a8718dfaaed39a2253887dcdf7cdadb1f8
Raghu Betina <raghu@firstdraft.com>
2018-12-03 22:44:25 +0000

    Only comparison_table user can access show page

--- app/controllers/comparison_tables_controller.rb
+++ app/controllers/comparison_tables_controller.rb
@@ -1,5 +1,5 @@
 class ComparisonTablesController < ApplicationController
-  before_action :current_user_must_be_comparison_table_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_comparison_table_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_comparison_table_user
     comparison_table = ComparisonTable.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
