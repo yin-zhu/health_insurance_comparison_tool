7c7601af455d6fed4a323a9ddaab1513d107d6fb
Raghu Betina <raghu@firstdraft.com>
2018-12-03 22:43:34 +0000

    Generate insurance_plans resource with draft:resource

---
+++ app/models/insurance_plan.rb
@@ -0,0 +1,8 @@
+class InsurancePlan < ApplicationRecord
+  # Direct associations
+
+  # Indirect associations
+
+  # Validations
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  # Routes for the Insurance plan resource:
+
+  # CREATE
+  get("/insurance_plans/new", { :controller => "insurance_plans", :action => "new_form" })
+  post("/create_insurance_plan", { :controller => "insurance_plans", :action => "create_row" })
+
+  # READ
+  get("/insurance_plans", { :controller => "insurance_plans", :action => "index" })
+  get("/insurance_plans/:id_to_display", { :controller => "insurance_plans", :action => "show" })
+
+  # UPDATE
+  get("/insurance_plans/:prefill_with_id/edit", { :controller => "insurance_plans", :action => "edit_form" })
+  post("/update_insurance_plan/:id_to_modify", { :controller => "insurance_plans", :action => "update_row" })
+
+  # DELETE
+  get("/delete_insurance_plan/:id_to_remove", { :controller => "insurance_plans", :action => "destroy_row" })
+
+  #------------------------------
+
   devise_for :users
   # Routes for the User resource:
 
